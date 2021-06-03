d2a9e8ecc5cae5922b1b8a53bf278dca8f11eef8
Michael Brock <michael.brock@dfaw.com.au>
2021-06-03 19:59:30 +1000

    added auth

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello from AWS</h1>
+        <h1>Now with Authorization!</h1>
       </header>
+
+      <AmplifySignOut />
     </div>
   );
 }
 
-export default App;
+export default withAuthenticator( App );
